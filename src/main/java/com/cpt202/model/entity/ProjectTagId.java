d34fde63a8e1b3ca86ce20b8f0a9bc8c47e1b105
Char1z@rd <crzhu.Richard@gmail.com>
2026-04-04 20:30:43 +0800

    Develop (#10)
    * chore(actions): rename workflows for clearer CI/CD distinction
    
    * refactor(entity): simplify entity classes using Lombok annotations (#8)
    
    * pom update
    
    * Dev/crzhu (#9)
    
    * refactor(entity): simplify entity classes using Lombok annotations
    
    * pom update
    
    * chore(workflows): rename CI/CD workflows for clarity

--- src/main/java/com/cpt202/model/entity/ProjectTagId.java
+++ src/main/java/com/cpt202/model/entity/ProjectTagId.java
@@ -2,52 +2,24 @@ package com.cpt202.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectTagId implements Serializable {
+    /** Project identifier in the composite key. */
     @Column(name = "project_id")
     private Long projectId;
 
+    /** Tag identifier in the composite key. */
     @Column(name = "tag_id")
     private Long tagId;
-
-    public ProjectTagId() {
-    }
-
-    public ProjectTagId(Long projectId, Long tagId) {
-        this.projectId = projectId;
-        this.tagId = tagId;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public Long getTagId() {
-        return tagId;
-    }
-
-    public void setTagId(Long tagId) {
-        this.tagId = tagId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ProjectTagId that)) return false;
-        return Objects.equals(projectId, that.projectId) &&
-                Objects.equals(tagId, that.tagId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(projectId, tagId);
-    }
 }
